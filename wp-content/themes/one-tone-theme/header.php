82315cda6053d91d9386157a7130f37c40bb12d9
Pauline Wahle <pauline.wahle@web.de>
2023-05-09 12:04:49 +0200

    header

---
+++ wp-content/themes/one-tone-theme/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewprt" content="width=device-width, initial-scale=1.0">
+    <title> <?php bloginfo('info'); ?> | <?php is_front_page() ? bloginfo('descripton') : wp_title(''); ?> </title>
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <header>
+        <nav>
+            <?php
+            wp_nav_menu(array(
+                'theme_location' => 'primary',
+                'menu_class' => 'primary-menu',
+            ));
+            ?>
+        </nav>
+    </header>
+
